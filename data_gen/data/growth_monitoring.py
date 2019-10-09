1390a9cbc62f47f0da3ec2124085841e93700d11
Cory Zue <czue@dimagi.com>
2019-10-09 12:04:12 +0200

    add functions / templates to randomize growth monitoring forms

---
+++ data_gen/data/growth_monitoring.py
@@ -0,0 +1,84 @@
+import json
+import os
+import datetime
+import random
+
+from . import randomizers
+
+
+def get_random_growth_monitoring_form():
+    template = get_growth_monitoring_form_template()
+    return randomize_template_values(template, random.Random("growth-monitoring"))
+
+
+def get_growth_monitoring_form_template():
+    template_filename = os.path.join(os.path.dirname(__file__), 'templates', 'growth-monitoring-form.json')
+    with open(template_filename, "r") as f:
+        template_string = f.read()
+
+    return template_string
+
+
+def randomize_template_values(template_string, random_generator):
+    wfa = randomizers.get_next_wfa(random_generator)
+    wfa_pos = abs(wfa)
+    child_id = randomizers.get_next_child_id(random_generator)
+    weight = randomizers.get_nextInt(random_generator)
+    zscore = randomizers.get_next_zscore(random_generator)
+    zscore_wfa = zscore * 3
+    zscore_icon = "red" if zscore_wfa < 3 else "yellow" if zscore_wfa < 2 else "green"
+    nutrition_status = "severely underweight" if zscore_wfa < 3 else "moderately underweight" if zscore_wfa < 2 else "normal"
+    datetime_modified_datetime = randomizers.get_next_datetime_modified(random_generator)
+    datetime_modified = datetime.datetime.strftime(datetime_modified_datetime, "%Y-%m-%dT%H:%M:%S.%fZ")
+    date_modified = datetime.datetime.strftime(datetime_modified_datetime, "%Y-%m-%d")
+    age_days = randomizers.get_next_age_days(random_generator)
+    age_weeks = age_days / 7.0
+    age_weeks_rounded = int(age_weeks)
+    age_months = age_days / 30.25
+    age_months_rounded = int(age_months)
+    age_dob = datetime.datetime.strftime(datetime_modified_datetime - datetime.timedelta(days = age_days),"%Y-%m-%d")
+    yes_no = randomizers.get_next_yes_no(random_generator)
+    # todo: add real location logic back
+    # location_dict = randomizers.get_next_location_dict(locationCycle)
+    location_dict = randomizers.get_random_location_dict(random_generator)
+    user_id = location_dict["location_id"]
+    username = location_dict["name"]
+    owner_id = location_dict["location_id"]
+    sex = randomizers.get_next_sex(random_generator)
+    form_id = randomizers.get_nextUUID(random_generator)
+    formXML_id = randomizers.get_nextUUID(random_generator)
+    deviceID = randomizers.get_next_device_id(random_generator)
+    child_name = randomizers.get_next_child_name(random_generator)
+    last_sync_token = randomizers.get_nextUUID(random_generator)
+    submit_ip = randomizers.get_next_ip(random_generator)
+
+    newForm = template_string % dict(
+        wfa=wfa,
+        child_id=child_id,
+        weight=weight,
+        zscore=zscore,
+        zscore_wfa=zscore_wfa,
+        zscore_icon=zscore_icon,
+        nutrition_status=nutrition_status,
+        age_days=age_days,
+        age_weeks=age_weeks,
+        age_weeks_rounded=age_weeks_rounded,
+        age_months=age_months,
+        age_months_rounded=age_months_rounded,
+        age_dob=age_dob,
+        yes_no=yes_no,
+        owner_id=owner_id,
+        sex=sex,
+        form_id=form_id,
+        user_id=user_id,
+        username=username,
+        formXML_id=formXML_id,
+        datetime_modified=datetime_modified,
+        date_modified=date_modified,
+        deviceID=deviceID,
+        child_name=child_name,
+        wfa_pos=wfa_pos,
+        last_sync_token=last_sync_token,
+        submit_ip=submit_ip
+    )
+    return json.loads(newForm)
---
+++ data_gen/data/randomizers.py
@@ -0,0 +1,84 @@
+import datetime
+import random
+import uuid
+
+
+RANDOM_EXAMPLE = random.Random("a")
+RANDOM_FORM1 = random.Random("form1")
+RANDOM_FORM2 = random.Random("form2")
+ACTIVITY_LIST = ["prayer_hygiene", "conversation", "cognitive", "physical_outdoor", "arts_crafts", "language", "packup"]
+MEAL_SERVED_LIST = ["cooked_served_hot", "cooked_served_cold", "packed_served_hot", "packed_served_cold", "not_served"]
+START_DATETIME = datetime.datetime.strptime("2018-07-01","%Y-%m-%d")
+END_DATETIME = datetime.datetime.strptime("2019-07-01","%Y-%m-%d")
+LOCATIONS_FILE = "locations.csv"
+CASE_LIST = []
+
+
+### Functions to get specific variables within forms
+def get_nextUUID(randomObj):
+    return str(uuid.UUID(int = randomObj.getrandbits(128)))
+
+def get_nextInt(randomObj):
+    return int(randomObj.random() * 10)
+
+def get_next_activity(randomObj):
+    return randomObj.sample(ACTIVITY_LIST,int(randomObj.random() * len(ACTIVITY_LIST)))
+
+def get_next_meal_served(randomObj):
+    return randomObj.sample(MEAL_SERVED_LIST,int(randomObj.random() * len(MEAL_SERVED_LIST)))
+
+def get_next_yes_no(randomObj):
+    if randomObj.random() < .5:
+        return "yes"
+    else:
+        return "no"
+
+def get_next_child_id(randomObj):
+    ## TO-DO: Fix this
+    return get_nextUUID(randomObj)
+
+def get_next_datetime_modified(randomObj):
+    delta_microseconds = (END_DATETIME - START_DATETIME).days * 24 * 60 * 60 * 1000000
+    randomObj.random()
+    return START_DATETIME + datetime.timedelta(microseconds=randomObj.random()*delta_microseconds)
+
+def get_next_gps_location(randomObj):
+    latitude = str(int(randomObj.random() * 180) - 90)
+    longitude = str(int(randomObj.random() * 360) - 180)
+    return latitude + " " + longitude + " 0.0 25.55"
+
+def get_next_device_id(randomObj):
+    return str(int(randomObj.random() * 1000000000000000))
+
+def get_next_ip(randomObj):
+    place1 = str(int(randomObj.random() * 256))
+    place2 = str(int(randomObj.random() * 256))
+    place3 = str(int(randomObj.random() * 256))
+    place4 = str(int(randomObj.random() * 256))
+    return place1 + "." + place2 + "." + place3 + "." + place4
+
+def get_next_child_name(randomObj):
+    ## TO-DO: Fix this
+    return get_nextUUID(randomObj)
+
+def get_next_wfa(randomObj):
+    return round(randomObj.random() * 20 - 10, 4)
+
+def get_next_zscore(randomObj):
+    return round(randomObj.random() * 6-3, 2)
+
+def get_next_age_days(randomObj):
+    return int(randomObj.random() * 1000)
+
+def get_next_sex(randomObj):
+    return "M" if randomObj.random() < 0.5 else "F"
+
+def get_next_location_dict(locationCycle):
+    nextRow = locationCycle.next()
+    return {"location_id": nextRow[0], "name": nextRow[2]}
+
+def get_random_location_dict(randomObj):
+    return {
+        'location_id': get_nextUUID(randomObj),
+        'name': get_next_child_name(randomObj),
+    }
