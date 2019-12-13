5a5815f02b333abc2bde24b6cfd7a4d81c85da2d
Cory Zue <czue@dimagi.com>
2019-12-13 10:13:46 +0200

    randomize immediate_breastfeeding

--- data_gen/data/birth_preparedness.py
+++ data_gen/data/birth_preparedness.py
@@ -1,6 +1,6 @@
 import json
 
-from data_gen.data.randomizers import get_next_uuid
+from data_gen.data.randomizers import get_next_uuid, get_next_yes_no
 from data_gen.data.util import get_template
 
 
@@ -14,6 +14,7 @@ def randomize_template_values(template_string, seed_values, visit_number):
     form_context = {
         'form_id': get_next_uuid(seed_values.random_instance),
         'submission_time': seed_values.context[seed_value_key],
+        'random_yes_no': get_next_yes_no(seed_values.random_instance),
     }
     form_context.update(seed_values.context)
     formatted_case = template_string % form_context
