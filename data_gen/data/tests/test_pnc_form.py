774c2d72cd860a85845172619a05b6daec46c307
Shubham Bansal <shubhambansal17@hotmail.com>
2020-01-02 09:29:03 +0530

    [refactor]: - pep8 styling             - var name fix             - commenting redundant test case
    - Github Issue:
    
    Authored-by: Shubham Bansal <shubhambansal17@hotmail.com>
    Signed-off-by: Shubham Bansal <shubhambansal17@hotmail.com>

--- data_gen/data/tests/test_pnc_form.py
+++ data_gen/data/tests/test_pnc_form.py
@@ -1,29 +1,31 @@
 from .base import DataGenTestBase
 
 
-class TestCaseRelationships(DataGenTestBase):
+class TestPNCForm(DataGenTestBase):
 
-  def test_pnc_form(self):
-    random_yes_no = ['yes', 'no']  # used to check bool values
-    while True:
-      data_generator = self.get_next_data_generator()
-      if data_generator.is_pregnant and not data_generator.change_phone_number:
-        data = list(data_generator.get_data())
-        self.assertEqual(8, len(data))
-        ccs_record_case = data[2]
-        pnc_form_data = data[6]
-        self.assertEqual(ccs_record_case.data['document']['case_id'],
-                         data_generator.seed_values.case_ids.pregnant_ccs_record)
-        self.assertEqual(ccs_record_case.data['document']['case_id'],
-                         pnc_form_data.data['document']['form']['case_load_ccs_record0']['case']['@case_id'])
-        self.assertNotEqual(
-            '%(form_id)s',
-            pnc_form_data.data['document']['form_id']
-        )
-        self.assertEqual(
-            pnc_form_data.data['document']['received_on'],
-            pnc_form_data.data['document']['form']['meta']['timeEnd'],
-        )
-        # bool fields here
-        self.assertIn(pnc_form_data.data['document']['form']['child']['item']['is_ebf'], random_yes_no)
-        break
+    def test_pnc_form(self):
+        yes_no_options = ['yes', 'no']  # used to check bool values
+        while True:
+            data_generator = self.get_next_data_generator()
+            if data_generator.is_pregnant and not data_generator.change_phone_number:
+                data = list(data_generator.get_data())
+                self.assertEqual(8, len(data))
+                ccs_record_case = data[2]
+                pnc_form_data = data[6]
+                # redundant case test
+                # note: try to avoid in further test cases
+                # self.assertEqual(ccs_record_case.data['document']['case_id'],
+                #                  data_generator.seed_values.case_ids.pregnant_ccs_record)
+                self.assertEqual(ccs_record_case.data['document']['case_id'],
+                                 pnc_form_data.data['document']['form']['case_load_ccs_record0']['case']['@case_id'])
+                self.assertNotEqual(
+                    '%(form_id)s',
+                    pnc_form_data.data['document']['form_id']
+                )
+                self.assertEqual(
+                    pnc_form_data.data['document']['received_on'],
+                    pnc_form_data.data['document']['form']['meta']['timeEnd'],
+                )
+                # bool fields here
+                self.assertIn(pnc_form_data.data['document']['form']['child']['item']['is_ebf'], yes_no_options)
+                break
